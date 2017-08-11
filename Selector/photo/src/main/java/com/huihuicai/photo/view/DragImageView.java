c23092a25dadac8ffef76a2d2bebdb1927c62bfa
huihuicai <1270174074@qq.com>
2017-08-11 11:43:19 +0800

    去掉log

--- Selector/photo/src/main/java/com/huihuicai/photo/view/DragImageView.java
+++ Selector/photo/src/main/java/com/huihuicai/photo/view/DragImageView.java
@@ -9,7 +9,6 @@ import android.os.Looper;
 import android.os.Message;
 import android.support.v7.widget.AppCompatImageView;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.ViewTreeObserver;
 
